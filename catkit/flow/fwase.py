426dcd04116bb61e261efecbb0de189c1956c1b4
Max Hoffmann <mjhoffmann@gmail.com>
2018-09-23 11:06:31 -0700

    Some refactorizations; moved calculator_parameters from atoms.info to atoms.info['calculator_parameters']

--- catkit/flow/fwase.py
+++ catkit/flow/fwase.py
@@ -1,20 +1,24 @@
-from . import fwio
-import functools
-import ase
+import importlib
+import json
+import copy
 import sys
+import ase
+
+from . import fwio
+
 try:
     import espresso
-except(ImportError):
-    pass
+except(ImportError, KeyError):
+    espresso = None
 
-
-def str_to_class(classname):
-    return functools.reduce(
-        getattr, classname.split('.'), sys.modules[__name__])
+def str_to_class(full_classname):
+    mod_name, classname =  full_classname.rsplit('.', 1)
+    return getattr(importlib.import_module(mod_name),
+                  classname)
 
 
 def get_potential_energy(
-        calculator,
+        calculator_name,
         in_file='input.traj',
         out_file='pw.pwo'):
     """Performs a ASE get_potential_energy() call with a compatible ase calculator.
@@ -35,21 +39,30 @@ def get_potential_energy(
     atoms = ase.io.read(in_file)
 
     # Setting up the calculator
-    calculator = str_to_class(calculator)
-    calc = calculator(atoms, **atoms.info)
+    calculator = str_to_class(calculator_name)
+    if calculator_name == 'espresso.Espresso':
+        calc = calculator(atoms, **atoms.info['calculator_parameters'])
+
+    else:
+        calc = calculator(**atoms.info)
+        atoms.set_calculator(calc)
 
     # Perform the calculation and write trajectory from log.
     atoms.get_potential_energy()
 
-    if isinstance(calc, espresso.Espresso):
+    if calculator_name == 'espresso.Espresso':
         # Patch for reading magmom of trajectory
         images = espresso.io.read(out_file, ':')
+    elif calculator_name == 'ase.calculators.emt.EMT':
+        images = copy.deepcopy(atoms)
     else:
         images = ase.io.read(out_file, ':')
 
     # Moneky patch for constraints
     for image in images:
-        image.constraints = atoms.constraints
-        image._pbc = atoms.pbc
+        if hasattr(image, 'constraints'):
+            image.constraints = atoms.constraints
+        if hasattr(atoms, 'pbc') and hasattr(image, '_pbc'):
+            image._pbc = atoms.pbc
 
     return fwio.atoms_to_encode(images)
